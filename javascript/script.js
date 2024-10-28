22ea4a7ea412153c8f940c250dd40822e7180152
antoniorusso2 <russoantonio303@outlook.it>
2024-10-28 15:49:02 +0100

    ADD: script for showing all team members automatically in cards

--- javascript/script.js
+++ javascript/script.js
@@ -38,3 +38,38 @@ const teamMembers = [
     img: "img/female3.png"
   }
 ];
+
+// elemento html row che conterra' le col con le card
+const row = document.getElementById('card-row');
+
+
+for (let i = 0; i < teamMembers.length; i++) {
+  // creazione div card
+  const card = document.createElement('div');
+  card.classList = 'card';
+
+  card.innerHTML =
+    `<div class="card-img">
+      <img src = "${teamMembers[i].img}" alt = "first member photo" >
+    </div >
+    <div class="card-body">
+      <a class="name" href="#">
+        ${teamMembers[i].name}
+      </a>
+
+      <p class="role">
+        ${teamMembers[i].role}
+      </p>
+
+      <a href="#">
+        <p class="e-mail">
+          ${teamMembers[i].email}
+        </p>
+      </a>
+
+    </div>
+  </div > `;
+
+  row.appendChild(card);
+
+}
